95c1c934f09034f0c94a9510409218bd7b41b550
Fuuma <masa81hiro69@gmail.com>
2024-05-16 16:20:53 +0900

    first commit

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,16 @@
+export default function SignIn({}) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Welcome to HogeHoge</h1> {/* アプリ名をここに追加 */}
+      <p>Please sign in to continue</p>
+    </div>
+  );
+}
